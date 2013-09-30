65f3bb0ed45926855e3d9304d9188c4d57b3c44a
Travis Hathaway <travis.j.hathaway@gmail.com>
2013-09-30 08:42:34 -0700

    making a couple small rearangements on fix_songs

--- fix_songs.py
+++ fix_songs.py
@@ -81,21 +81,20 @@ def main():
     # Grab args
     args = docopt(__doc__)
 
-    if args.get('--recurse'):
-        for top in args.get('<directory>'):
-            for walk in os.walk(top):
-                for fname in walk[2]:
-                    file_name = os.path.join(walk[0],fname)
+    for top in args.get('<directory>'):
+        if os.path.isdir(top):
+            if args.get('--recurse'):
+                for walk in os.walk(top):
+                    for fname in walk[2]:
+                        file_name = os.path.join(walk[0],fname)
 
-                    if fname[-3:].lower() == 'wma':
-                        rename_wma( file_name )
-                    elif fname[-3:].lower() == 'mp3':
-                        rename_mp3( file_name )
-                    elif fname[-3:].lower() == 'm4a':
-                        rename_m4a( file_name )
-    else:
-        for top in args.get('<directory>'):
-            if os.path.isdir(top):
+                        if fname[-3:].lower() == 'wma':
+                            rename_wma( file_name )
+                        elif fname[-3:].lower() == 'mp3':
+                            rename_mp3( file_name )
+                        elif fname[-3:].lower() == 'm4a':
+                            rename_m4a( file_name )
+            else:
                 for fname in os.listdir(top):
                     file_name = os.path.join(top,fname)
 
@@ -105,17 +104,17 @@ def main():
                         rename_mp3( file_name )
                     elif fname[-3:].lower() == 'm4a':
                         rename_m4a( file_name )
-            elif os.path.isfile(top):
-                file_name = top
+        elif os.path.isfile(top):
+            file_name = top
 
-                if file_name[-3:].lower() == 'wma':
-                    rename_wma( file_name )
-                elif file_name[-3:].lower() == 'mp3':
-                    rename_mp3( file_name )
-                elif file_name[-3:].lower() == 'm4a':
-                    rename_m4a( file_name )
-            else:
-                print('"'+top + '" is neither a file nor directory')
+            if file_name[-3:].lower() == 'wma':
+                rename_wma( file_name )
+            elif file_name[-3:].lower() == 'mp3':
+                rename_mp3( file_name )
+            elif file_name[-3:].lower() == 'm4a':
+                rename_m4a( file_name )
+        else:
+            print('"'+top + '" is neither a file nor directory')
 
 
 if __name__=='__main__':
